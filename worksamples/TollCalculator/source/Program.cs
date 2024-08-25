bf9fb34796dcdea26a508884c872abafa0e65789
magdulator <gusta@DESKTOP-IBUVBMH>
2024-08-25 14:17:47 +0200

    fix: runnable version of tollcalculator

---
+++ worksamples/TollCalculator/source/Program.cs
@@ -0,0 +1,13 @@
+﻿using TollFeeCalculator;
+
+var tollCalculator = new TollCalculator();
+DateTime[] dates = {
+                    new DateTime(2024, 8, 26, 7, 0, 0), //18
+                   new DateTime(2024, 8, 26, 7, 59, 0), // 0 (within one hour)
+                   new DateTime(2024, 8, 26, 10, 0, 0), //8
+                   new DateTime(2024, 8, 26, 10, 30, 0), //0 (within one hour)
+                   new DateTime(2024, 8, 26, 19, 0, 0) }; //0
+                                                          //== 26
+var vehicle = new Car();
+
+Console.WriteLine(tollCalculator.GetTollFee(vehicle, dates));
